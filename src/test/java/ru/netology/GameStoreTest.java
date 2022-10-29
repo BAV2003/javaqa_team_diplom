1ef58004e8de6538d1f2c7e84aa1b854019a34a0
Andrei Bezrukov <bezrukov.andrey86@gmail.com>
2022-10-29 17:34:12 +0500

    Add tests in GameStoreTest

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
@@ -15,4 +17,120 @@ public class GameStoreTest {
     }
 
     // другие ваши тесты
+
+    @Test
+    public void playedTimeTest() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Andrei");
+        player.installGame(game);
+        player.play(game, 1);
+
+        int expected = 1;
+        int actual = store.getSumPlayedTime();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void addPlayTimeTest() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player = new Player("Andrei");
+        player.installGame(game);
+        player.play(game, 1);
+        store.addPlayTime("Andrei", 2);
+
+        int expected = 3;
+        int actual = store.getSumPlayedTime();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void mostPlayedPlayerTest() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player1 = new Player("Andrei");
+        Player player2 = new Player("Evgenia");
+        player1.installGame(game);
+        player1.play(game, 1);
+        player2.installGame(game);
+        player2.play(game, 2);
+
+        String expected = "Evgenia";
+        String actual = store.getMostPlayer();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void sumPlayedTimeTest() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Кольца", "Аркады");
+
+        Player player1 = new Player("Andrei");
+        Player player2 = new Player("Evgenia");
+        player1.installGame(game1);
+        player1.play(game1, 1);
+        player2.installGame(game2);
+        player2.play(game2, 2);
+
+        int expected = 3;
+        int actual = store.getSumPlayedTime();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void playedTimeManyGamesTest() {
+
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Кольца", "Аркады");
+        Game game3 = store.publishGame("Утки", "Стратегия");
+
+        Player player = new Player("Andrei");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+        player.play(game1, 1);
+        player.play(game2, 4);
+        player.play(game3, 7);
+
+        int expected = 12;
+        int actual = store.getSumPlayedTime();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void noMostPlayedPlayerTest() {
+
+        GameStore store = new GameStore();
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+
+        Player player1 = new Player("Andrei");
+        Player player2 = new Player("Evgenia");
+        player1.installGame(game);
+        player2.installGame(game);
+
+        String expected = null;
+        String actual = store.getMostPlayer();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
 }
